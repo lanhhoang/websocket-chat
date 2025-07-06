e1f33980da081076aedabc254cf62dd137eb6757
Lanh Hoang <lanhhoang@users.noreply.github.com>
2025-07-05 21:14:41 -0400

    Add socket connection setup and toast notifications in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,44 @@
+import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { io } from "socket.io-client";
 
 import IndexPage from "./pages/IndexPage";
 import LoginPage from "./pages/LoginPage";
+import makeToast from "./Toaster";
 
 import "./App.css";
 
 function App() {
+  const [socket, setSocket] = useState(null);
+
+  const setupSocket = () => {
+    if (!socket) {
+      const newSocket = io("http://localhost:3000/chat", {
+        transports: ["websocket"],
+      });
+
+      newSocket.on("connect", () => {
+        console.log("Socket connected:", newSocket.id);
+        makeToast(
+          "success",
+          `Connected to the server with ID: ${newSocket.id}`
+        );
+      });
+
+      newSocket.on("disconnect", () => {
+        console.log("Socket disconnected");
+        setSocket(null);
+        makeToast("error", "Disconnected from the server");
+      });
+
+      setSocket(newSocket);
+    }
+  };
+
+  useEffect(() => {
+    setupSocket();
+  }, []);
+
   return (
     <Router>
       <Switch>
