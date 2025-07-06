bd657144ed54d67840c38f3c06ddf51eab3edc6e
Lanh Hoang <lanhhoang@users.noreply.github.com>
2025-07-05 22:24:29 -0400

    Add DashboardPage component to display user conversations

---
+++ src/pages/DashboardPage.jsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+import makeToast from "../Toaster";
+
+const DashboardPage = () => {
+  const [conversations, setConversations] = useState([]);
+  const getConversations = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/conversations", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+        },
+      });
+      console.log("Conversations response:", response);
+      if (response.status === 200) {
+        setConversations(response.data);
+      } else {
+        console.error("Failed to fetch conversations:", response.data.message);
+      }
+    } catch (error) {
+      console.error("Error fetching conversations:", error);
+      makeToast(
+        "error",
+        error?.response?.data?.message ||
+          "An error occurred while fetching conversations"
+      );
+    }
+  };
+
+  useEffect(() => {
+    getConversations();
+  }, []);
+
+  return (
+    <div className="card">
+      <div className="cardHeader">Conversations</div>
+      <div className="chatrooms">
+        {conversations.map((conversation) => (
+          <div key={conversation._id} className="chatroom">
+            <div>Conversation {conversation._id}</div>
+            <Link to={"/conversations/" + conversation._id}>
+              <div className="join">Join</div>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default DashboardPage;
